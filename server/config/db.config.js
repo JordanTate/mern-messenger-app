ac62d8d59c594683c9bc8133bf443be93f3f93a0
JordanTate <JordanTateAdds@iCloud.com>
2023-07-05 19:58:05 +0100

    Add db.config.js

---
+++ server/config/db.config.js
@@ -0,0 +1,13 @@
+// Packages
+import mysql from 'mysql';
+
+// Create Connection
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'messenger',
+});
+
+export default db;
