d97a8bd35b748fa8153fa21b82cc0c8fced0e3cf
JordanTate <JordanTateAdds@iCloud.com>
2023-07-15 16:27:14 +0100

    Update app to use  package in order to parse HttpOnly cookies from request object

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // Packages
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import "./config/env.config.js";
 
 // Route Handlers
@@ -13,6 +14,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
