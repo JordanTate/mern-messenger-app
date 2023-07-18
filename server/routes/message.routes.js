89c925f78ecbbb11ab79d0b6678d7ea67ec94818
JordanTate <JordanTateAdds@iCloud.com>
2023-07-18 19:40:18 +0100

    Update messageController function from  to  for readability. Add  function to handle the user sending new messages

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -2,14 +2,15 @@
 import express from "express";
 
 // Import Controllers
-import handleMessages from "../controllers/message.controller.js";
+import { getMessages, postMessage } from "../controllers/message.controller.js";
 import { verifyAuth } from "../controllers/auth.controller.js";
 
 // Set Express Router
 const router = express.Router();
 
 // Routes
-router.get('/', verifyAuth, handleMessages);
+router.get('/', verifyAuth, getMessages);
+router.post('/', verifyAuth, postMessage);
 
 // Export Router
 export default router;
